133506f230a243d2ed7abf86b2b1e87456dc335f
edm1001 <edmer_franciz@yahoo.com>
2023-09-01 13:43:22 -0500

    api route changes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,9 @@ const {User} = require('../../models');
 // GET /api/users
 router.get('/', (req, res) => {
     // access user model and run findAll
-    User.findAll()
+    User.findAll({
+        attributes: {exclude: ['password'] }
+    })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
@@ -15,13 +17,15 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res)=> {
     User.findOne({
+        attributes: {exclude: ['password']},
         where: {
             id: req.params.id
         }
     })
     .then(dbUserData => {
         if (!dbUserData) {
-            res.status(404).json({message: 'No user found with this id'})
+            res.status(404).json({message: 'No user found with this id'});
+            return;
         }
         res.json(dbUserData)
     })
@@ -85,4 +89,6 @@ router.delete('/:id', (req, res)=> {
           console.log(err);
           res.status(500).json(err);
         });
-})
+});
+
+module.exports = router;
